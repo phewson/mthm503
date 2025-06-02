158250a297027f162a37d035bcc1a86fde85b3fa
Paul Hewson <p.hewson2@exeter.ac.uk>
2025-06-02 15:50:02 +0100

    Update sqlite test database

--- mock_data/mock_df.R
+++ mock_data/mock_df.R
@@ -7,4 +7,14 @@ mock_df <- data.frame(
 )
 
 DBI::dbWriteTable(con, "stats19_casualties", mock_df, overwrite = TRUE)
+
+mock_extrications <- data.frame(
+  financial_year = c("2010/11", "2011/12"),
+  extrication = c("Unknown", "Hydraulic"),
+  n_casualties = c(10, 20),
+  sex = c("Male", "Female"),
+  age_band = c("0-16", c("0-16"))
+)
+
+DBI::dbWriteTable(con, "fire_rescue_extrication_casualties", mock_df, overwrite = TRUE)
 DBI::dbDisconnect(con)
