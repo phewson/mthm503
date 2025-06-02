2e582d34185171255b9a058b8a37fd525c178525
Paul Hewson <p.hewson2@exeter.ac.uk>
2025-06-02 15:31:40 +0100

    Fix lints

--- R/clean_extrications.R
+++ R/clean_extrications.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 age_bands <- c("0-16", "17-24", "25-39", "40-64", "65+", "Unknown")
 financial_years <- c("2010/11", "2011/12", "2012/13", "2013/14", "2014/15",
                      "2015/16", "2016/17", "2017/18", "2018/19", "2019/20")
@@ -5,8 +7,10 @@ financial_years <- c("2010/11", "2011/12", "2012/13", "2013/14", "2014/15",
 
 clean_extrications <- function(df) {
   df %>%
-    mutate(extrication = as.factor(extrication), sex = as.factor(sex), 
-           age_band = factor(age_band, levels = age_bands),
-           financial_year = factor(financial_year, levels = financial_years,
-                                   ordered = TRUE)) 
+    mutate(.data$extrication <- as.factor(.data$extrication),
+           .data$sex <- as.factor(.data$sex),
+           .data$age_band <- factor(.data$age_band, levels = age_bands),
+           .data$financial_year <- factor(.data$financial_year,
+                                          levels = financial_years,
+                                          ordered = TRUE))
 }
